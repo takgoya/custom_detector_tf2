61029d51bc74fd890c6e7e2d92bece7431e80960
takgoya <javier.goya@gmail.com>
2021-01-28 10:31:28 +0100

    updated ocr recognition

--- ocr_plate_recognition.py
+++ ocr_plate_recognition.py
@@ -31,13 +31,15 @@ def convert_grayscale(image):
 '''
 Remove noise
 '''
-def remove_noise(image):
-    return cv2.GaussianBlur(image, (5,5), 0)
+def remove_noise(image, kernel_size=5):
+    #return cv2.GaussianBlur(image, (kernel_size,kernel_size), 0)
+    return cv2.medianBlur(image, kernel_size)
+
 '''
 Threshold
 '''
 def binary_thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+    return cv2.threshold(image, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
 '''
 Erosion
@@ -74,35 +76,21 @@ def get_contours(image):
     return sorted_contours
 
 '''
-Bitwise
+Background
 '''
-def bitwise_operator(operator, image):
-    image_copy = image.copy()
-    if operator == "and":
-        image_copy = cv2.bitwise_and(image_copy)
-    elif operator == "not":
-        image_copy = cv2.bitwise_not(image_copy)
-        
-    return image_copy
-
-'''
-Roi
-'''
-def get_roi(image, x, y, w, h):
-    # grab character region of image
-    roi = image[y-5:y+h+5, x-5:x+w+5]
-    
-    # perfrom bitwise not to flip image to black text on white background
-    roi = bitwise_operator("not", roi)
-
-    return roi
+def is_black_background(image):
+    is_black = True
+    image_h, image_w = image.shape[:2]
+    if cv2.countNonZero(image) > ((image_w*image_h)//2):
+        is_black = False
+    return is_black
 
 '''
 Tesseract options
 '''
-def tesseract_options(psm=8, oem=1):
-    # Page segmentation modes: 8 = Treat the image as a single word.
-    # OCR Engine modes: 1 = Neural nets LSTM engine only.
+def tesseract_options(psm, oem):
+    # Page Segmentation Modes
+    # OCR Engine modes
     alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     options = "-c tessedit_char_whitelist={}".format(alphanumeric)
     options += " --psm {}".format(psm)
@@ -113,8 +101,12 @@ def tesseract_options(psm=8, oem=1):
 Recognize plate
 '''
 def recognize_plate(image):
+    # create blank string to hold license plate number
+    plate_num = ""
+
     # resize image to three times as large as original for better readability
     image_resized = resize_image(image, 3, 3)
+    image_h, image_w = image_resized.shape[:2]
     #cv2.imwrite("media/output/matricula_resized.jpg", image_resized)
     
     # convert to grayscale
@@ -126,34 +118,33 @@ def recognize_plate(image):
     #cv2.imwrite("media/output/matricula_blurred.jpg", blurred)
 
     # create rectangular kernel for morphological operations
-    rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+    rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
 
-    # apply erosion to make regions more clear
-    erosion = erosion_morph(blurred, rect_kern, iterations = 1)
+    erosion = erosion_morph(blurred, rect_kern)
     #cv2.imwrite("media/output/matricula_erosion.jpg", erosion)
 
     # threshold the image using Otsus method to preprocess for tesseract
-    ret, thresh = binary_thresholding(erosion)
+    ret, thresh = binary_thresholding(erosion)    
     #cv2.imwrite("media/output/matricula_thresh.jpg", thresh)
     
-    opening = opening_morph(thresh, rect_kern)
-    #cv2.imwrite("media/output/matricula_opening.jpg", opening)
+    dilation = erosion_morph(thresh, rect_kern)
+    #cv2.imwrite("media/output/matricula_erosion.jpg", erosion)
 
+    # check if bitwise is needed
+    if is_black_background(dilation):
+        dilation = cv2.bitwise_not(dilation)
+
+    '''
     # find contours of regions of interest within license plate
     contours = get_contours(thresh)
-    
-    # create blank string to hold license plate number
-    plate_num = ""
-    
+
     i = 0
     # loop through contours and find individual letters and numbers in license plate
     for cnt in contours:
         x,y,w,h = cv2.boundingRect(cnt)
 
-        height, width = image_resized.shape[:2]
-        
         # filter by height of the contour
-        if (h / height) < 0.5 or (h / height) > 0.75:
+        if w >h or w*h < 100 or image_h/h > 3:
             continue
         
         i = i+1
@@ -163,21 +154,39 @@ def recognize_plate(image):
         #cv2.imwrite("media/output/matricula_contours.jpg", gray)
         
         # region of interest
-        roi = get_roi(thresh, x, y, w, h)
+        roi = dilation[y-7:y+h+7, x-7:x+w+7]
+        #if is_black:
+        #    roi = cv2.bitwise_not(roi)
         
         # show each roi as an img
-        #roi_filename = "media/output/roi"+str(i)+".jpg"
-        #cv2.imwrite(roi_filename, roi)
+        roi_filename = "media/output/roi"+str(i)+".jpg"
+        cv2.imwrite(roi_filename, roi)
         
         # tesseract character recognition
-        options = tesseract_options(psm=8, oem=1)
-        text = pytesseract.image_to_string(roi, config=options)
+        options = tesseract_options(psm=8, oem=3)
+        try:
+            text = pytesseract.image_to_string(roi, config=options)
         
+            # clean tesseract text by removing any unwanted blank spaces
+            clean_text = re.sub('[\W_]+', '', text)
+            plate_num += clean_text
+        except:
+            text = None
+    
+    '''
+    
+    # tesseract character recognition
+    options = tesseract_options(psm=7, oem=3)
+    try:
+        text = pytesseract.image_to_string(dilation, config=options)
+
         # clean tesseract text by removing any unwanted blank spaces
         clean_text = re.sub('[\W_]+', '', text)
         plate_num += clean_text
-        
-    if plate_num != "":
-        print("[INFO] recognize_plate ... plate = {}".format(plate_num))
-    
-    return(plate_num)
+    except:
+        text = None
+
+    if len(plate_num) >= 6:
+        return(plate_num)
+    else:
+        return ("")
