0ab4ee6c6cc2cb1753ea4d16d87bd57935528c60
takgoya <javier.goya@gmail.com>
2021-01-30 00:36:25 +0100

    TF2 Object Detection for video

--- object_detection_video.py
+++ object_detection_video.py
@@ -35,9 +35,16 @@ import argparse
 import json
 import pathlib
 import time
+import datetime
 
 # utils functions for tesseract ocr plate recognition
 import ocr_plate_recognition
+
+# utils functions for db
+from db import db_utils
+
+# utils functions for gps
+from gps import gps_utils
 
 '''
 Arguments
@@ -51,9 +58,14 @@ args = vars(ap.parse_args())
 conf = json.load(open(args["conf"]))
 
 '''
+GPS
+'''
+if (conf["use_gps"]):
+    gps_socket, data_stream = gps_utils.init_gps()
+
+'''
 Load model and labels
 '''
-
 print("[INFO] loading model ...")
 start_time = time.time()
 # Load saved model and build the detection function
@@ -91,13 +103,37 @@ except:
     total = -1
 
 '''
+Prepare DB
+'''
+# Create (if not exists) DB connection
+conn = db_utils.create_connection(conf["db"])
+
+if conn != None:
+    # Create (if not exists) RECORDINGS table
+    db_utils.create_recordings_table(conn)
+    # Create (if not exists) DETECTIONS table
+    db_utils.create_detections_table(conn)
+    print("[INFO] DB configured")
+else:
+    print("[INFO] error while configuring DB")
+
+# Generate recording entry name
+recording_name = datetime.datetime.now().strftime("%d%m%Y-%H%M%S")
+# Insert recording into RECORDINGS table
+recording_id = db_utils.insert_recording(conn, recording_name)
+    
+'''
 Read frames in the loop
 '''
-# variable for counting frames
-f = 0
-# variable for counting total time
-t = 0
+# Name for generated videofile
+recording_path = conf["video_output"] + "/" + recording_name + ".mp4" 
 
+# variable for counting frames
+f_count = 0
+
+# variable for counting time
+start_time = time.time()
+    
 # loop over frames from the video file stream
 while True:
     # read the next frame from the file
@@ -115,8 +151,6 @@ while True:
 
     frame_height, frame_width = frame.shape[:2]
 
-    start = time.time()
-    
     '''
     Run inference
     '''
@@ -148,6 +182,9 @@ while True:
     nms_indices = tf.image.non_max_suppression(boxes, scores, length, conf["threshold"])
     nms_boxes = tf.gather(boxes, nms_indices)
     
+    # dictionary for db
+    db_detections = {}
+    
     for i in range(len(scores)):
         if ((scores[i] > conf["threshold"]) and (scores[i] <= 1.0)):
             # Get bounding box coordinates and draw box
@@ -158,63 +195,82 @@ while True:
             ymax = int(min(frame_height,(nms_boxes[i][2] * frame_height)))
             xmax = int(min(frame_width,(nms_boxes[i][3] * frame_width)))
 
+            '''
+            # distance
+            distance = (2 * 3.14 * 180) / (ymax + xmax * 360) * 1000 + 3 ### Distance measuring in Inch
+            distance_cm = distance * 2.54
+            '''            
             cv2.rectangle(frame_np, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
             # Draw label
             # Look up object name from "labels" array using class index
             object_name = category_index[int(classes[i])]['name']
-            label = '%s: %d%%' % (object_name, int(scores[i]*100))
-            #label = "%s" % (object_name)
+            label = "%s: %d%%" % (object_name, int(scores[i]*100))
+            #label+= " y distancia = %f cms" % (distance_cm)
+            
             labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2) # Get font size
             label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
             cv2.rectangle(frame_np, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (10, 255, 0), cv2.FILLED)
 
             # Draw white box to put label text in
             cv2.putText(frame_np, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2) # Draw label text           '''
-
+            
+            plate_num = ""
             #Extract the detected number plate
-            if object_name == "plate":
+            if object_name == "licence":
                 licence_img = frame_np[ymin:ymax, xmin:xmax]
-                text = ocr_plate_recognition.recognize_plate(licence_img)
-                cv2.putText(frame_np, text, (xmin, ymax + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (10, 255, 0), 2)
-                #cv2.imwrite('matricula_reconocida.jpg', licence_img)
-
-    end = time.time()
+                image_h, image_w = licence_img.shape[:2]
+                if image_w != 0 and image_h != 0:
+                    plate_num = ocr_plate_recognition.recognize_plate(licence_img)
+                    cv2.putText(frame_np, plate_num, (xmin, ymax + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (10, 255, 0), 2)
+                    if plate_num != "":
+                        print("[INFO] licence recognition = {}".format(plate_num))
+                        if (i-1) >= 0:
+                            db_detections[plate_num] = category_index[int(classes[i-1])]['name']
+                        else:
+                            db_detections[plate_num] = category_index[int(classes[i+1])]['name']
+                                                    
+    elapsed_time = round(time.time() - start_time, 2)
     
     # increase counters for frames and total time
-    f += 1
-    t += end - start
-    
+    f_count += 1
+        
     '''
     Write processed frame into file
     '''
     if writer is None:
         # initialize video writer
         fourcc = cv2.VideoWriter_fourcc(*conf["video_codec"])
-        writer = cv2.VideoWriter(conf["video_output"], fourcc, 32, (frame_np.shape[1], frame_np.shape[0]), True)
-        
-        # some information on processing single frame
-        if total > 0:
-            elap = (end - start)
-            print("[INFO] single frame took {:.4f} seconds".format(elap))
-            print("[INFO] estimated total time to finish: {:.4f}".format(elap * total))
+        writer = cv2.VideoWriter(recording_path, fourcc, 24, (frame_np.shape[1], frame_np.shape[0]), True)
 
     # write processed current frame to the file
     writer.write(frame_np)
+    
+    '''
+    Insert into DB
+    '''
+    # get gps position
+    gps_lat = gps_lon = 0
+    if (conf["use_gps"]):
+        gps_lat, gps_lon = gps_utils.get_position(gps_socket, data_stream)
 
-    key = cv2.waitKey(1) & 0xFF
-    # if the "Esc" key was pressed, break from the loop
-    if key == (ord("q")) or key == 27:
-        break
+    # get detection time
+    detection_datetime = datetime.datetime.now().strftime("%d%m%Y-%H%M%S")
+
+    # add entry to db
+    for key, value in db_detections.items():
+        detection = (recording_id, value, key, gps_lat, gps_lon, elapsed_time, f_count, detection_datetime)
+        db_utils.insert_detection(conn, detection)
 
 '''
 Finish
 '''
+end_time = time.time()
+
 # print final results
-print()
-print("[INFO] total number of frames", f)
-print("[INFO] total amount of time {:.5f} seconds".format(t))
-print("[INFO] fps:", round((f / t), 1))
+print("[INFO] total number of frames", f_count)
+print("[INFO] total amount of time {:.5f} seconds".format(end_time - start_time))
+print("[INFO] fps:", round((f_count / (end_time - start_time)), 1))
 print()
 
 # do a bit of cleanup
